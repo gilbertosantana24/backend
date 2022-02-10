5263708028d5559d3f054d65b19badfba18d91da
gilbertosantana24 <gilbertobuja@gmail.com>
2022-02-09 17:41:08 -0800

    package

--- database.js
+++ database.js
@@ -1,7 +1,13 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'netflix-bd',
-    DIALECT: 'mysql'
-}
+    HOST: 'ec2-34-204-128-77.compute-1.amazonaws.com',
+    USER: 'mxzpbrobmgtxjf',
+    PASSWORD: 'bc5c4a46560862f30547e740cf888c51c47b105398dbbd08e855a2ae356e4f5a',
+    DB: 'd8bamg2lan7284',
+    DIALECT: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+};
