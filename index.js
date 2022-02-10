7080d3868617639858f4cf6d2d24497028205c52
gilbertosantana24 <gilbertobuja@gmail.com>
2022-02-09 18:14:11 -0800

    port

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ require("./routes/movie.routes")( app );
 
 
 //start server + port
-app.listen( 3000, () => {
-    console.log("SERVER IS RUNNING!!")
-})
+const PORT = process.env.PORT || 8070;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
